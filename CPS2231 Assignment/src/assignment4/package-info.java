3f07ae5bfa0c47362bffa100c4e09e6ae80edc0c
NaSummer <na_summer@icloud.com>
2015-05-03 15:27:08 +0800

    Create assignment info
    Create assignment info using pacage-info.java

---
+++ CPS2231 Assignment/src/assignment4/package-info.java
@@ -0,0 +1,102 @@
+/**
+ * Section 10.1-10.14, 5 problems
+Due:  2015/5/11
+*/
+//===================================================================
+/**
+Project 1: The Pet Class
+Problem Description: 
+	 Create a class named Pet. 
+1.	Analysis: 
+	Some String data fields named name, species, breed, gender
+	Some String Array data fields named color, character
+	A constructor that creates a Pet for no information
+	A constructor that creates a Pet for all the information
+	The methods isDog() and isMale() that return true if the Pet is dog and male respectively. 
+	The methods containColor(color) and containCharacter(character) that return true if the Pet contains such color and character respectively. 
+2. Design: 
+	Make a UML class diagram for it. 
+3. Coding and Testing:
+	(Do not forget to Test it)
+
+4. Submit:
+	Compress all your work in to a Zip file and submit it to BlackBoard. 
+*/
+//===================================================================
+/**
+Project 2: FindPet
+Problem Description:
+	Use the Pet class that you just made. Create several pets and find a special pet. 
+1. Analysis
+	Create some pets with their information
+	Ask user’s request for the pet
+	Find the special pet
+2.	Design
+3. Coding and Testing:
+	(Do not forget to Test it)
+Test Example: 
+ 
+4. Submit:
+	Compress all your work in to a Zip file and submit it to BlackBoard.  
+*/
+//===================================================================
+/**
+Project 3: The MyDate Class
+Problem Description:
+	Design a class named MyDate.
+1.	Analysis: 
+	The data fields year, month and day that represent a date. (1 for January)
+	A no-arg constructor that creates a MyDate object for the current date. 
+	A constructor that constructs a My Date object with a specified elapsed time since midnight, January 1, 1970, in milliseconds. 
+	A constructor that constructs a MyDate object with the specified year, month, and day. 
+	Three get methods for the data fields year, month, and day, respectively.
+	A method named setDate(long elapsedTime) that sets a new date for the object using the elapsed time. 
+2. Design
+Draw the UML diagram for the calss. 
+(You may use the GregorianCalendar class to simplify your code.)
+3. Coding and Testing:
+	(Do not forget to Test it)
+Write a Test class that creates two MyDate objects using three different kinds of constructors and displays their year, month and day. 
+For Example: 
+ 
+4. Submit:
+Compress all your work in to a Zip file and submit it to BlackBoard. 
+*/
+//===================================================================
+/**
+Project 4: Divisible By 2 Or 3
+Problem Description:
+	Find the numbers with 50 decimal digits that are divisible by 2 or 3. 
+2.	Analysis: 
+Use BigInteger to make it. 
+2. Design
+	Ask how many numbers do the user want.
+	Display the numbers.
+3. Coding and Testing:
+	(Do not forget to Test it)
+Test Examples:
+ 
+4. Submit:
+Compress all your work in to a Zip file and submit it to BlackBoard. 
+*/
+//===================================================================
+/**
+Project 5: Using StackOfIntegers Class
+Problem Description:
+	Write a program that displays all the prime numbers less than 120 in decreasing order. Use the StackOfIntegers class to store the prime numbers (e.g., 2,3,5…) and retrieve and display them in reverse order. 
+3.	Analysis: 
+Here is the StackOfIntegers class in the book: 
+http://www.cs.armstrong.edu/liang/intro9e/html/StackOfIntegers.html
+2. Design
+3. Coding and Testing:
+	(Do not forget to Test it)
+ 
+4. Submit:
+Compress all your work in to a Zip file and submit it to BlackBoard.
+
+ */
+/**
+ * @author Qiufan
+ *
+ */
+package assignment4;
