f7762f4cb346d63f4c8df8df6b6c0f2f075df5b2
NaSummer <na_summer@icloud.com>
2015-05-05 00:28:48 +0800

    Create cps2231_ass4_proj4
    Create cps2231_ass4_proj4 first time
    Solution:
    BigIntger 转换为一个数组，其中一个一位，
    2：看最后一位能不能除2余0
    3：数组求和，看和能不能除3余0

---
+++ CPS2231 Assignment/src/assignment4/DivisibleBy2Or3.java
@@ -0,0 +1,45 @@
+package assignment4;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+class DivisibleBy2Or3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// ask for the number of numbers
+		System.out.println("How many numbers do you want?");
+		long numOfNum = sc.nextLong();
+		Random random = new Random(System.currentTimeMillis());
+		
+		// output loop
+		while (numOfNum>0) {
+			// generate a BigInteger
+			BigInteger num = new BigInteger(50,random);
+			// change the BigInteger into a byte array
+			byte[] numInByteArr = num.toByteArray();
+			
+			// divisible by 2
+			if (numInByteArr[numInByteArr.length-1] % 2 == 0) {
+				System.out.println(num);
+				numOfNum--;
+				continue;
+			}
+			
+			// divisible by 3
+			long sumOfByte = 0;
+			for (int j = 0; j < numInByteArr.length; j++) {
+				sumOfByte += numInByteArr[j];
+			}
+			if (sumOfByte%3==0) {
+				System.out.println(num);
+				numOfNum--;
+				continue;
+			}
+		}
+		
+	}
+
+}
