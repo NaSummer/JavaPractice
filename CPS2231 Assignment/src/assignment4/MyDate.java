e1d7e4c667a6dd670724f72e5a1a1655918df6ae
NaSummer <na_summer@icloud.com>
2015-05-06 00:23:50 +0800

    [cps2231_ass4_proj3] enhance #13
    Using GregorianCalendar to check the validity in MyDate(y,m,d).
    More reliable.
    Reduce the complexity of the code.

--- CPS2231 Assignment/src/assignment4/MyDate.java
+++ CPS2231 Assignment/src/assignment4/MyDate.java
@@ -37,9 +37,10 @@ class MyDate {
 	}
 	
 	public MyDate(int year, int month, int day) {
-		setYear(year);
-		setMonth(month);
-		setDay(day);
+		GregorianCalendar GCdate = new GregorianCalendar(year, month-1, day);
+		this.year = GCdate.get(Calendar.YEAR);
+		this.month = GCdate.get(Calendar.MONTH)+1;
+		this.day = GCdate.get(Calendar.DATE);
 	}
 	
 	// =============setter==============
@@ -49,64 +50,6 @@ class MyDate {
 		this.year = GCdate.get(Calendar.YEAR);
 		this.month = GCdate.get(Calendar.MONTH)+1;
 		this.day = GCdate.get(Calendar.DATE);
-	}
-	
-	// =========private setter==========
-	private void setYear(int year) {
-		this.year = year;
-	}
-
-	private void setMonth(int month) {
-		if ( (month>=1)&&(month<=12) ) {
-			this.month = month;
-		} else {
-			System.err.println("Enter the wrong date.");
-		}
-	}
-
-	private void setDay(int day) {
-		if (this.month>0) {
-			
-			switch (this.month) {
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:
-				if (day<=31) {
-					this.day = day;
-				} else {
-					System.err.println("Enter the wrong date.");
-				}
-				break;
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if (day<=30) {
-					this.day = day;
-				} else {
-					System.err.println("Enter the wrong date.");
-				}
-				break;
-			case 2:
-				if ( ((isLeapYear(this.year))&&(day<=29)) || ((!isLeapYear(this.year))&&(day<=28)) ) {
-					this.day = day; 
-				} else {
-					System.err.println("Enter the wrong date.");
-				}
-				break;
-			default:
-				System.err.println("Enter the wrong date.");
-				break;
-			}
-			
-		} else {			
-			System.err.println("Enter the wrong date.");
-		}
-
 	}
 	
 	// =============getter==============
@@ -120,23 +63,6 @@ class MyDate {
 
 	public int getDay() {
 		return this.day;
-	}
-	
-	// ============Tool=================
-	private boolean isLeapYear(int year) {
-		if (year%4==0) {
-			if (year%100==0) {
-				if (year%400==0) {
-					return true;
-				} else {
-					return false;
-				}
-			} else {
-				return true;
-			}
-		} else {
-			return false;
-		}
 	}
 	
 }
