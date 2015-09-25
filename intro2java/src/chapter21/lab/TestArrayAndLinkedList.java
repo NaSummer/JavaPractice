cac4ef106b88750a92167e11728a4f5584848d3c
QiufanXu <519893120@qq.com>
2015-09-25 12:18:36 +0800

    In Class Example intro2Java ch21 Generic
    Generic

---
+++ intro2java/src/chapter21/lab/TestArrayAndLinkedList.java
@@ -0,0 +1,43 @@
+package chapter21.lab;
+/**
+ * @author Qiufan(Andy) Xu 
+ * @date CreateTime: Sep 25, 2015 10:04:38 AM 
+ * @version 1.0 
+ * @param 
+ */
+
+import java.util.*;
+
+public class TestArrayAndLinkedList {
+
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * @param 
+		 * @return 
+		 */
+		List<Integer> arrayList = new ArrayList<Integer>();
+	    arrayList.add(1); // 1 is autoboxed to new Integer(1)
+	    arrayList.add(2);
+	    arrayList.add(3);
+	    arrayList.add(1);
+	    arrayList.add(4);
+	    arrayList.add(0, 10);
+	    arrayList.add(3, 30);
+
+	    System.out.println("A list of integers in the array list:");
+	    System.out.println(arrayList);
+
+	    LinkedList<Object> linkedList = new LinkedList<Object>(arrayList);
+	    linkedList.add(1, "red");
+	    linkedList.removeLast();
+	    linkedList.addFirst("green");
+
+	    System.out.println("Display the linked list backward:");
+	    for (int i = linkedList.size() - 1; i >= 0; i--) {
+	      System.out.print(linkedList.get(i) + " ");
+	    }
+	    
+	}
+
+}
